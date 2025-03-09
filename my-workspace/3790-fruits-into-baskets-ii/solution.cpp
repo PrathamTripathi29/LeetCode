cf5be973adf955dbe98e489521b3d3edd06ab8f9
PrathamTripathi <prathamtripathi29@gmail.com>
2025-03-09 18:12:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.9 MB (75.90%)

---
+++ my-workspace/3790-fruits-into-baskets-ii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int count = 0;
+        for(int i=0; i<fruits.size(); i++){
+            for(int j=0; j<baskets.size(); j++){
+                if(baskets[j] >= fruits[i]){
+                    baskets[j] = -1;
+                    count++;
+                    break;
+                }
+            }
+        }
+        return fruits.size() - count;
+    }
+};
