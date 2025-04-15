10481de18922cd38e782e5114238a61e45e583b6
PrathamTripathi <prathamtripathi29@gmail.com>
2025-04-15 16:49:48 +0000

    Sync LeetCode submission Runtime - 2 ms (4.93%), Memory - 8.4 MB (50.07%)

---
+++ my-workspace/0070-climbing-stairs/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    unordered_map<int,int> mp;
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        if(mp.find(n) == mp.end()){
+            mp[n] = climbStairs(n-1) + climbStairs(n-2);
+        }
+        return mp[n];
+    }
+};
