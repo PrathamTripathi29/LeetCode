ed44b22c9f9ab4883b75f201abeebb38515dbea1
PrathamTripathi <prathamtripathi29@gmail.com>
2025-07-17 09:43:25 +0000

    Sync LeetCode submission Runtime - 4 ms (73.38%), Memory - 11.7 MB (65.83%)

--- my-workspace/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-workspace/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l = s.length();
+        int l = 0;
         int maxl = 0;
-        unordered_map<char,int> mp;
-        int left = 0;
-        for(int i=0; i<l; i++){
-            if(mp.count(s[i]) == 0 || mp[s[i]] < left){
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.length(); i++){
+            if(mp.find(s[i]) == mp.end() || mp[s[i]] < l){
                 mp[s[i]] = i;
-                maxl = max(maxl, i - left + 1);
-            }
-            else{
-                left = mp[s[i]] + 1;
+                maxl = max(maxl, i-l+1);
+            } else {
+                l = mp[s[i]] + 1;
                 mp[s[i]] = i;
             }
         }
