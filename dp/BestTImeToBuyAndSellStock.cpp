44a3673c63937e5bc68c710312467ba29a912e83
PrathamTripathi <prathamtripathi29@gmail.com>
2025-09-06 11:59:34 +0530

    First Commit

---
+++ dp/BestTImeToBuyAndSellStock.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int min = prices[0];
+        int profit = 0;
+        for (int price : prices)
+        {
+            if (price > min)
+                profit = max(profit, price - min);
+            if (price < min)
+            {
+                min = price;
+            }
+        }
+        return profit;
+    }
+};
---
+++ dp/WlidcardMathing.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    bool isMatch(string s, string p)
+    {
+        int sl = s.length();
+        int pl = p.length();
+        vector<vector<int>> dp(sl + 1, vector<int>(pl + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= sl; i++)
+            dp[i][0] = 0;
+        for (int i = 1; i <= pl; i++)
+        {
+            int flag = 1;
+            for (int k = 1; k <= i; k++)
+            {
+                if (p[k - 1] != '*')
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            dp[0][i] = flag;
+        }
+        int j;
+        for (int i = 1; i <= sl; i++)
+        {
+            for (j = 1; j <= pl; j++)
+            {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p[j - 1] == '*')
+                {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[sl][pl];
+    }
+};
