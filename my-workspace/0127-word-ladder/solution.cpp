81e82b3f21728f58694933435a0ae60d0c971520
PrathamTripathi <prathamtripathi29@gmail.com>
2025-12-27 10:01:39 +0000

    Sync LeetCode submission Runtime - 49 ms (81.42%), Memory - 21.2 MB (60.91%)

--- my-workspace/0127-word-ladder/solution.cpp
+++ my-workspace/0127-word-ladder/solution.cpp
@@ -1,63 +1,31 @@
 class Solution {
 public:
-    bool diffcheck(string a, string b){
-        int diff = 0;
-        for(int i=0; i<a.length(); i++){
-            if(a[i] != b[i]){
-                diff++;
-            }
-        }
-        if(diff > 1){
-            return false;
-        }
-        return true;
-    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        if(beginWord == endWord){
-            return 1;
+        unordered_set<string> s;
+        for(int i=0; i<wordList.size(); i++){
+            s.insert(wordList[i]);
         }
-        int n = wordList.size();
-        bool flag = true;
-        unordered_map<string, bool> vis;
-        vis[beginWord] = false;
-        for(int i=0; i<n; i++){
-            if(wordList[i] == endWord){
-                flag = false;
-            }
-            vis[wordList[i]] = false;
-        }
-        if(flag){
-            return 0;
-        }
-
-        unordered_map<string, vector<string>> mp;
-        
-        for(int i=0; i<n; i++){
-            if(diffcheck(beginWord, wordList[i])){
-                mp[beginWord].push_back(wordList[i]);
-            }
-        }
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(diffcheck(wordList[i], wordList[j])){
-                    mp[wordList[i]].push_back(wordList[j]);
-                    mp[wordList[j]].push_back(wordList[i]);
-                }
-            }
-        }
-
         queue<pair<string, int>> q;
         q.push({beginWord, 1});
+        s.erase(beginWord);
         while(!q.empty()){
             string word = q.front().first;
-            int currDist = q.front().second;
-            vis[word] = true;
+            int dist = q.front().second;
             q.pop();
-            for(auto it : mp[word]){
-                if(!vis[it])
-                    q.push({it, currDist+1});
-                if(it == endWord)
-                    return currDist + 1;
+            if(word == endWord){
+                return dist;
+            }
+            for(int i=0; i<word.length(); i++){
+                char oric = word[i];
+                for(char c = 'a'; c <= 'z'; c++){
+                    if(c == oric) continue;
+                    word[i] = c;
+                    if(s.count(word)){
+                        s.erase(word);
+                        q.push({word, dist+1});
+                    }
+                }
+                word[i] = oric;
             }
         }
         return 0;
