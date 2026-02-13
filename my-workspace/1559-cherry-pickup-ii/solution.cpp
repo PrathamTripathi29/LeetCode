dc72f91f40098e3bf406892b8faa7ea8c9143000
PrathamTripathi <prathamtripathi29@gmail.com>
2026-02-13 13:14:15 +0000

    Sync LeetCode submission Runtime - 47 ms (62.92%), Memory - 21.1 MB (12.91%)

--- my-workspace/1559-cherry-pickup-ii/solution.cpp
+++ my-workspace/1559-cherry-pickup-ii/solution.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    int cherryPickup(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
-        for(int j1=0; j1<c; j1++){
-            for(int j2=0; j2<c; j2++){
-                if(j1 == j2){
-                    dp[r-1][j1][j2] = grid[r-1][j1];
-                } else{
-                    dp[r-1][j1][j2] = grid[r-1][j1] + grid[r-1][j2];
-                }
-            }
-        }
-
-        for(int i=r-2; i>=0; i--){
-            for(int j1=0; j1<c; j1++){
-                for(int j2=0; j2<c; j2++){
-                    int maxcherries = 0;
-                    for(int k1 = -1; k1<=1; k1++){
-                        for(int k2=-1; k2<=1; k2++){
-                            if(j1+k1>=0 && j1+k1 <c && j2+k2>=0 && j2+k2 <c){
-                                maxcherries = max(maxcherries, dp[i+1][j1+k1][j2+k2]);
-                            }
-                        }
+    vector<int> dc1 = {-1, 0, 1};
+    vector<int> dc2 = {-1, 0, 1};
+    int f(int r, int c1, int c2, vector<vector<vector<int>>>& dp, vector<vector<int>>& grid, int m, int n){
+        if(r == m) return 0;
+        if(r < 0) return 0;
+        if(c1 < 0 || c2 < 0 || c1 >= n || c2 >= n) return 0;
+        if(dp[r][c1][c2] != -1){
+            return dp[r][c1][c2];
+        } else {
+            if(c1 == c2){
+                int maxVal = 0;
+                for(int i=0; i<3; i++){
+                    for(int j=0; j<3; j++){
+                        int nc1 = c1+dc1[i];
+                        int nc2 = c2+dc2[j];
+                        maxVal = max(maxVal, f(r+1, nc1, nc2, dp, grid, m, n));
                     }
-                    int currcherries = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2]; 
-                    dp[i][j1][j2] = maxcherries + currcherries;
                 }
+                return dp[r][c1][c2] = maxVal + grid[r][c1];
+            } else {
+                int maxVal = 0;
+                for(int i=0; i<3; i++){
+                    for(int j=0; j<3; j++){
+                        int nc1 = c1+dc1[i];
+                        int nc2 = c2+dc2[j];
+                        maxVal = max(maxVal, f(r+1, nc1, nc2, dp, grid, m, n));
+                    }
+                }
+                return dp[r][c1][c2] = maxVal + grid[r][c1] + grid[r][c2];
             }
         }
-
-        return dp[0][0][c-1];
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        return f(0, 0, n-1, dp, grid, m, n);
     }
 };
