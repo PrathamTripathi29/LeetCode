f6b9f6fb23bdca06957bf0bdef7b3d00156be7a1
PrathamTripathi <prathamtripathi29@gmail.com>
2026-02-16 12:37:48 +0000

    Sync LeetCode submission Runtime - 4 ms (75.09%), Memory - 14.2 MB (76.19%)

--- my-workspace/0300-longest-increasing-subsequence/solution.cpp
+++ my-workspace/0300-longest-increasing-subsequence/solution.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int f(int idx, int prev, int n, vector<int>& nums, vector<vector<int>>& dp){
-        if(idx == n) return 0;
-        if(dp[idx][prev+1] != -1){
-            return dp[idx][prev+1];
-        } else {
-            int notTake = f(idx+1, prev, n, nums, dp);
-            int take = 0;
-            if(prev == -1 || nums[idx] > nums[prev]){
-                take = f(idx+1, idx, n, nums, dp) + 1;
-            }
-            return dp[idx][prev+1] = max(take, notTake);
-        }
-    }
+    // int f(int idx, int prev, int n, vector<int>& nums, vector<vector<int>>& dp){
+    //     if(idx == n) return 0;
+    //     if(dp[idx][prev+1] != -1){
+    //         return dp[idx][prev+1];
+    //     } else {
+    //         int notTake = f(idx+1, prev, n, nums, dp);
+    //         int take = 0;
+    //         if(prev == -1 || nums[idx] > nums[prev]){
+    //             take = f(idx+1, idx, n, nums, dp) + 1;
+    //         }
+    //         return dp[idx][prev+1] = max(take, notTake);
+    //     }
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return f(0, -1, n, nums, dp);
+        // int n = nums.size();
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // return f(0, -1, n, nums, dp);
+        vector<int> ans;
+        for(int num : nums){
+            if(ans.empty() || ans.back() < num){
+                ans.push_back(num);
+            } else {
+                auto it = lower_bound(ans.begin(), ans.end(), num);
+                *it = num;
+            }
+        }
+        int len = ans.size();
+        return len;
     }
 };
