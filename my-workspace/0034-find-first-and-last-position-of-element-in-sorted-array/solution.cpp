f4510d494bec7d75d23f004105b7cb83150f39fc
PrathamTripathi <prathamtripathi29@gmail.com>
2026-02-22 10:30:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (18.78%)

--- my-workspace/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-workspace/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int l = 0;
-        int h = n-1;
+        int l=0;
+        int h=n-1;
+        int foccur = n, loccur=n;
         while(l <= h){
             int mid = l + (h-l)/2;
-            if(nums[mid] == target){
-                int st = mid;
-                int ed = mid;
-                while(st >= 0 && nums[st] == target){
-                    st--;
-                }
-                st++;
-                while(ed < n && nums[ed] == target){
-                    ed++;
-                }
-                ed--;
-                return {st, ed};
-            }
-            if(target > nums[mid]){
-                l = mid+1;
-            } else {
+            if(nums[mid] >= target){
                 h = mid-1;
+                foccur = mid;
+            } else{
+                l = mid + 1;
             }
         }
-        return {-1, -1};
+        l=0;
+        h=n-1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] > target){
+                h = mid-1;
+                loccur = mid;
+            } else{
+                l = mid + 1;
+            }
+        }
+        if(foccur >= n || nums[foccur] != target){
+            return {-1, -1};
+        }
+        return {foccur, loccur-1};
     }
 };
