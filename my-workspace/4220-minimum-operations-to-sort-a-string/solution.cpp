9a2ff374d911d899651974c864a76d88525a442c
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-09 09:18:46 +0000

    Sync LeetCode submission Runtime - 3 ms (96.97%), Memory - 27.5 MB (71.52%)

--- my-workspace/4220-minimum-operations-to-sort-a-string/solution.cpp
+++ my-workspace/4220-minimum-operations-to-sort-a-string/solution.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
     int minOperations(string s) {
-        string str = s;
-        string str2 = s;
-        int n = str.length();
-        if(n == 1){
-            return 0;
+        char mini, maxi;
+        mini = 127;
+        maxi = 0;
+        int n = s.length();
+        if(is_sorted(s.begin(), s.end())) return 0;
+        if(n == 2) return -1;
+        for(int i=1; i<n-1; i++){
+            char ch = s[i];
+            mini = min(mini, ch);
+            maxi = max(maxi, ch);
         }
-        if(n == 2){
-            if(s[0] > s[1]){
-                return -1;
-            }
-        }
-        sort(str.begin(), str.begin() + n-1);
-        int ans1 = 0;
-        if(s.substr(0, n-1) != str.substr(0, n-1)){
-            ans1++;
-        }
-        if(str[n-1] < str[n-2]){
-            ans1++;
-        }
-        sort(str.begin()+1, str.end());
-        if(str[0] > str[1]){
-            ans1++;
-        }
-        int ans2 = 0;
-        sort(str2.begin()+1, str2.end());
-        if(s.substr(1, n) != str2.substr(1, n)){
-            ans2++;
-        }
-        if(str2[0] > str2[1]){
-            ans2++;
-        }
-        sort(str2.begin(), str2.begin() + n-1);
-        if(str2[n-1] < str2[n-2]){
-            ans2++;
-        }
-        
-        return min(ans1, ans2);
+        if(s[0] <= s[n-1] && (mini >= s[0] || maxi <= s[n-1])) return 1;
+        if(s[0] > maxi && s[n-1] < mini) return 3;
+        return 2; 
     }
 };
