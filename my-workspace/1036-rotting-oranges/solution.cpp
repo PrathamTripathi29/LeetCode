1e66ca48b56d5a66d8b7a29d217adfa5efbc8a3c
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-11 16:30:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.7 MB (86.06%)

--- my-workspace/1036-rotting-oranges/solution.cpp
+++ my-workspace/1036-rotting-oranges/solution.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int mins = -1;
+        int ans = -1;
         int fresh = 0;
-        int r = grid.size();
-        int c = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         queue<pair<int, int>> q;
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(grid[i][j] == 2){
-                    q.push({i,j});
-                } else if(grid[i][j] == 1) {
+                    q.push({i, j});
+                }  
+                if(grid[i][j] == 1){
                     fresh++;
-                }
+                } 
             }
         }
         if(fresh == 0) return 0;
-        int dr[4] = {1, 0, -1, 0};
-        int dc[4] = {0, 1, 0, -1};
-
         while(!q.empty()){
             int size = q.size();
             while(size--){
-                pair<int,int> p = q.front();
+                pair<int, int> orange = q.front();
                 q.pop();
+                int dr[4] = {1, -1, 0, 0};
+                int dc[4] = {0, 0, 1, -1};
                 for(int i=0; i<4; i++){
-                    int nr = p.first + dr[i];
-                    int nc = p.second + dc[i];
-                    if(nr >= 0 && nc >= 0 && nr < r && nc < c && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
-                        fresh--;
-                        q.push({nr, nc});
+                    int nr = orange.first + dr[i];
+                    int nc = orange.second + dc[i];
+                    if(nr >= 0 && nc >= 0 && nr < m && nc < n){
+                        if(grid[nr][nc] == 1){
+                            grid[nr][nc] = 2;
+                            q.push({nr, nc});
+                            fresh--;
+                        }
                     }
                 }
             }
-            mins++;
+            ans++;
         }
-        return fresh == 0 ? mins : -1;
+        if(fresh){
+            return -1;
+        }
+        return ans;
     }
 };
