821ba1d27e2550e390d93f45153673593bf1194f
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-11 15:10:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (76.84%)

--- my-workspace/0733-flood-fill/solution.cpp
+++ my-workspace/0733-flood-fill/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &image, int sr, int sc, int color, int stcolor){
+    void f(int sr, int sc, vector<vector<int>>& image, int oriColor, int color, int m, int n){
         image[sr][sc] = color;
-        int d1[4] = {1, 0, -1, 0};
-        int d2[4] = {0, 1, 0, -1};
+        int dr[4] = {1, -1, 0, 0};
+        int dc[4] = {0, 0, 1, -1};
         for(int i=0; i<4; i++){
-            int dr = sr + d1[i];
-            int dc = sc + d2[i];
-            if(dr >= 0 && dc >= 0 && dr < image.size() && dc < image[0].size()){
-                if(image[dr][dc] == stcolor && image[dr][dc] != color){
-                    dfs(image, dr, dc, color, stcolor);
+            int nr = sr + dr[i];
+            int nc = sc + dc[i];
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n){
+                if(image[nr][nc] != color && image[nr][nc] == oriColor){
+                    f(nr, nc, image, oriColor, color, m, n);
                 }
             }
+            
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int stcolor = image[sr][sc];
-        dfs(image, sr, sc, color, stcolor);
+        int oriColor = image[sr][sc];
+        int m = image.size();
+        int n = image[0].size();
+        f(sr, sc, image, oriColor, color, m, n);
         return image;
     }
 };
