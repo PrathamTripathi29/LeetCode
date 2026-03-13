27a0dc3b12882214d3499fce58cd6162d89a0a5e
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-13 14:10:32 +0000

    Sync LeetCode submission Runtime - 3 ms (23.02%), Memory - 14.6 MB (49.78%)

--- my-workspace/0130-surrounded-regions/solution.cpp
+++ my-workspace/0130-surrounded-regions/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    void dfs(int r, int c, int m, int n, vector<vector<char>>& board, vector<vector<int>>& vis){
+    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& vis, int m, int n){
         vis[r][c] = 1;
-        vector<int> dr = {0, 0, 1, -1};
-        vector<int> dc = {-1, 1, 0, 0};
+        int dr[4] = {1, -1, 0, 0};
+        int dc[4] = {0, 0, 1, -1};
         for(int i=0; i<4; i++){
             int nr = r + dr[i];
             int nc = c + dc[i];
-            if(nr >= 0 && nc >= 0 && nr < m && nc < n && !vis[nr][nc] && board[nr][nc] == 'O'){
-                dfs(nr, nc, m, n, board, vis);
+            if(nr >= 0 && nc >= 0 && nr < m && nc < n){
+                if(board[nr][nc] == 'O'){
+                    if(!vis[nr][nc])
+                        dfs(nr, nc, board, vis, m, n);
+                }
             }
         }
     }
@@ -16,22 +19,28 @@ public:
         int m = board.size();
         int n = board[0].size();
         vector<vector<int>> vis(m, vector<int>(n, 0));
+        for(int i=0; i<n; i++){
+            if(board[0][i] == 'O'){
+                dfs(0, i, board, vis, m, n);
+            }
+            if(board[m-1][i] == 'O'){
+                dfs(m-1, i, board, vis, m, n);
+            }
+        }
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i == 0 || i == m-1 || j == 0 || j == n-1){
-                    if(board[i][j] == 'O'){
-                        dfs(i, j, m, n, board, vis);
-                    }
-                }
+            if(board[i][0] == 'O'){
+                dfs(i, 0, board, vis, m, n);
+            }
+            if(board[i][n-1] == 'O'){
+                dfs(i, n-1, board, vis, m, n);
             }
         }
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(!vis[i][j] && board[i][j] == 'O'){
+                if(board[i][j] == 'O' && !vis[i][j]){
                     board[i][j] = 'X';
                 }
             }
         }
-
     }
 };
