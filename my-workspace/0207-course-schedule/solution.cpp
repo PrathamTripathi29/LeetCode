256540eb102ce3713e8ccd0da5e2a5880e94dcb1
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-26 19:00:20 +0000

    Sync LeetCode submission Runtime - 3 ms (75.31%), Memory - 19.4 MB (54.52%)

--- my-workspace/0207-course-schedule/solution.cpp
+++ my-workspace/0207-course-schedule/solution.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>> &adj, vector<int> &vis, vector<int> &pathVis){
-        vis[node] = 1;
-        pathVis[node] = 1;
-        for(int nbr : adj[node]){
-            if(!vis[nbr]){
-                if(dfs(nbr, adj, vis, pathVis) == false){
-                    return false;
-                }
-            }else if(pathVis[nbr]){
-                return false;
-            }
-        }
-        pathVis[node] = 0;
-        return true;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        vector<vector<int>> adj(n);
-        vector<int> vis(n, 0);
-        vector<int> pathVis(n, 0);
+        vector<vector<int>> adj(numCourses);
+        vector<int> inDegree(numCourses, 0);
         for(int i=0; i<prerequisites.size(); i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            adj[b].push_back(a);
+            inDegree[a]++;
         }
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                if(dfs(i, adj, vis, pathVis) == false){
-                    return false;
-                }
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
             }
         }
-        return true;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(int nbr : adj[node]){
+                inDegree[nbr]--;
+                if(inDegree[nbr] == 0) q.push(nbr);
+            }
+        }
+        if(ans.size() == numCourses) return true;
+        return false;
+        
     }
 };
