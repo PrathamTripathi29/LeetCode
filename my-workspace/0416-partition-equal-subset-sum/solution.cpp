7ce57f93c48017b27fae27cd777b5a34d4a89cc8
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-29 20:40:57 +0000

    Sync LeetCode submission Runtime - 171 ms (22.08%), Memory - 110.5 MB (31.53%)

--- my-workspace/0416-partition-equal-subset-sum/solution.cpp
+++ my-workspace/0416-partition-equal-subset-sum/solution.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+    bool f(int idx, int sum, vector<int>& nums, vector<vector<int>>& dp, int n){
+        if(idx == n-1) return nums[idx] == sum;
+        if(dp[idx][sum] != -1) return dp[idx][sum];
+        bool take = false;
+        if(nums[idx] <= sum){
+            take = f(idx+1, sum-nums[idx], nums, dp, n);
+        }
+        bool notTake = f(idx+1, sum, nums, dp, n);
+        return dp[idx][sum] = take || notTake;
+    }
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int sum = 0;
-        for(int num : nums){
-            sum += num;
-        }
-        if(sum %2 != 0) return false;
-        int targetSum = sum/2;
-        vector<bool> dp(targetSum + 1, false);
-        dp[0] = true;
-        for(int num : nums){
-            for(int i=targetSum; i>=num; i--){
-                if(dp[i-num]) dp[i] = true;
-                if(dp[targetSum]) return true;
-            }
-        }
-        return dp[targetSum];
+        for(int num : nums) sum += num;
+        if(sum%2) return false;
+        sum /= 2;
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return f(0, sum, nums, dp, n);
     }
 };
