74441aaee3a5dc0b9259f901910d04d01d7b3ade
PrathamTripathi <prathamtripathi29@gmail.com>
2026-03-31 06:30:42 +0000

    Sync LeetCode submission Runtime - 56 ms (61.50%), Memory - 21.1 MB (72.99%)

--- my-workspace/0127-word-ladder/solution.cpp
+++ my-workspace/0127-word-ladder/solution.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> s;
-        for(int i=0; i<wordList.size(); i++){
-            s.insert(wordList[i]);
+        unordered_set<string> words;
+        for(auto word : wordList){
+            words.insert(word);
         }
         queue<pair<string, int>> q;
         q.push({beginWord, 1});
-        s.erase(beginWord);
         while(!q.empty()){
-            string word = q.front().first;
-            int dist = q.front().second;
-            q.pop();
+            auto [word, dist] = q.front();
             if(word == endWord){
                 return dist;
             }
+            words.erase(word);
+            q.pop();
             for(int i=0; i<word.length(); i++){
-                char oric = word[i];
+                char oc = word[i];
                 for(char c = 'a'; c <= 'z'; c++){
-                    if(c == oric) continue;
+                    if(c == oc) continue;
                     word[i] = c;
-                    if(s.count(word)){
-                        s.erase(word);
-                        q.push({word, dist+1});
+                    if(words.count(word)){
+                        words.erase(word);
+                        q.push({word, dist + 1});
                     }
                 }
-                word[i] = oric;
+                word[i] = oc;
             }
         }
         return 0;
