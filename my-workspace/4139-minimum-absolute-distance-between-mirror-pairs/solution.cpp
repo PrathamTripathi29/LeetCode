c6adaa85fa26926bd2c8297eaa121c58361d8ea7
PrathamTripathi <prathamtripathi29@gmail.com>
2026-04-17 06:04:55 +0000

    Sync LeetCode submission Runtime - 114 ms (50.04%), Memory - 131.9 MB (41.56%)

--- my-workspace/4139-minimum-absolute-distance-between-mirror-pairs/solution.cpp
+++ my-workspace/4139-minimum-absolute-distance-between-mirror-pairs/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int reversenum(int num){
-        int rev = 0;
+    int reverse(int num){
+        int revnum = 0;
         while(num > 0){
-            rev = rev*10 + (num%10);
+            revnum *= 10;
+            revnum += num%10;
             num /= 10;
         }
-        return rev;
+        return revnum;
     }
     int minMirrorPairDistance(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int, int> mp;
-        int mindist = INT_MAX;
-        for(int i=0; i<nums.size(); i++){
-            int num = nums[i];
-            auto it = mp.find(num);
-            if(it != mp.end()){
-                mindist = min(mindist, abs(it->second - i));
+        int minDist = INT_MAX;
+        for(int i=0; i<n; i++){
+            int revnum = reverse(nums[i]);
+            if(mp.find(nums[i]) != mp.end()){
+                minDist = min(minDist, i - mp[nums[i]]);
             }
-            int rev = reversenum(num);
-            mp[rev] = i;
+            mp[revnum] = i;
         }
-        if(mindist == INT_MAX) return -1;
-        return mindist;
+        return minDist == INT_MAX ? -1 : minDist;
     }
 };
