b0b0bca594f38082f7d7b8043f59f110545f24b3
PrathamTripathi <prathamtripathi29@gmail.com>
2026-05-15 14:32:50 +0000

    Sync LeetCode submission Runtime - 9 ms (92.47%), Memory - 26.1 MB (46.26%)

--- my-workspace/1171-shortest-path-in-binary-matrix/solution.cpp
+++ my-workspace/1171-shortest-path-in-binary-matrix/solution.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
+            return -1;
+        }
         vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
         dist[0][0] = 1;
-        queue<tuple<int, int, int>> pq;
-        pq.push({1, 0, 0});
-        int dr[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+        int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-        while(!pq.empty()){
-            auto [d, r, c] = pq.front();
-            pq.pop();
-            if(d > dist[r][c]) continue;
+
+        // dist, row, col
+
+        queue<tuple<int, int, int>> q;
+        q.push({1, 0, 0});
+        while(!q.empty()){
+            auto [d ,r, c] = q.front();
+            q.pop();
             for(int i=0; i<8; i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0){
-                    if(d + 1 < dist[nr][nc]){
-                        dist[nr][nc] = d + 1;
-                        pq.push({dist[nr][nc], nr, nc});
-                    }
+                if(nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0 && d+1 < dist[nr][nc]){
+                    q.push({d + 1, nr, nc});
+                    dist[nr][nc] = d+1;
                 }
             }
         }
-        return dist[n-1][n-1] == INT_MAX ? -1 : dist[n-1][n-1];
+        if(dist[n-1][n-1] == INT_MAX) return -1;
+        return dist[n-1][n-1];
     }
 };
