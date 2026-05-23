a65c85bbf9597596defd838daeb005490fb772b2
PrathamTripathi <prathamtripathi29@gmail.com>
2026-05-23 10:44:27 +0000

    Sync LeetCode submission Runtime - 46 ms (23.44%), Memory - 36.7 MB (38.68%)

--- my-workspace/0518-coin-change-ii/solution.cpp
+++ my-workspace/0518-coin-change-ii/solution.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    int f(int idx, int amount, vector<int>& coins, vector<vector<int>>& dp, int n){
+        if(idx == n){
+            if(amount == 0) return 1;
+            return 0;
+        }
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int notTake = f(idx+1, amount, coins, dp, n);
+        int take = 0;
+        if(coins[idx] <= amount){
+            take = f(idx, amount-coins[idx], coins, dp, n);
+        }
+        return dp[idx][amount] = take + notTake;
+    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<unsigned long long> dp(amount+1, 0);
-        dp[0] = 1;
-        for(int coin : coins){
-            for(int i = coin; i<= amount; i++){
-                dp[i] = dp[i] + dp[i-coin];
-            }
-        }
-        return (int)dp[amount];
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        return f(0, amount, coins, dp, n);
     }
 };
