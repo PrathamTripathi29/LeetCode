fcb7690e7de6291e7ecd389b5523ea6d00756794
PrathamTripathi <prathamtripathi29@gmail.com>
2026-05-23 05:11:09 +0000

    Sync LeetCode submission Runtime - 4 ms (76.63%), Memory - 15.1 MB (58.31%)

--- my-workspace/0494-target-sum/solution.cpp
+++ my-workspace/0494-target-sum/solution.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    int f(int idx, int sum, vector<int>& nums, vector<vector<int>>& dp, int n){
+        if(idx == n){
+            if(sum == 0) return 1;
+            return 0;
+        }
+        if(dp[idx][sum] != -1) return dp[idx][sum];
+        int notTake = f(idx+1, sum, nums, dp, n);
+        int take = 0;
+        if(nums[idx] <= sum){
+            take = f(idx+1, sum-nums[idx], nums, dp, n);
+        }
+        return dp[idx][sum] = take + notTake;
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int sum = 0;
-        for(int num : nums){
-            sum += num;
+        int arrsum = 0;
+        for(auto num : nums){
+            arrsum += num;
         }
-        if(sum < abs(target) || (target+sum)%2 != 0) return 0;
-
-        int newtar = (target+sum)/2;
-
-        vector<int> dp(newtar+1, 0);
-        dp[0] = 1;
-        for(int num : nums){
-            for(int i = newtar; i >= num; i--){
-                dp[i] += dp[i-num];
-            }
-        }
-        return dp[newtar];
-
+        if(arrsum < abs(target) || (arrsum + target)%2 != 0) return 0;
+        int sum = (arrsum + target)/2;
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return f(0, sum, nums, dp, n);
     }
 };
