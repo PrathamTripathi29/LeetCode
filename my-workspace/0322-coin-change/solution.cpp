0ecfcd31e05cae2a4dab529f289b2865e27a4912
PrathamTripathi <prathamtripathi29@gmail.com>
2026-05-23 04:52:23 +0000

    Sync LeetCode submission Runtime - 44 ms (37.41%), Memory - 52.8 MB (14.74%)

--- my-workspace/0322-coin-change/solution.cpp
+++ my-workspace/0322-coin-change/solution.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1, 1e8);
-        dp[0] = 0;
-        int amountleft = amount;
-        for(int i=0; i<n; i++){
-            for(int j = coins[i]; j<=amount; j++){
-                dp[j] = min(dp[j], dp[j-coins[i]] + 1);
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 1e8));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                int notTake = dp[i-1][j];
+                int take = 1e8;
+                if(coins[i-1] <= j){
+                    take = dp[i][j-coins[i-1]] + 1;
+                }
+                dp[i][j] = min(take, notTake);
             }
         }
-        return dp[amount] == 1e8 ? -1 : dp[amount];
+        return dp[n][amount] == 1e8 ? -1 : dp[n][amount];
     }
 };
