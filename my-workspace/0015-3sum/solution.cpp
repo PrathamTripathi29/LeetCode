7e15d016d28a1dae9c73613ce252fb6c3834e726
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-01 17:26:20 +0000

    Sync LeetCode submission Runtime - 51 ms (43.99%), Memory - 28.9 MB (92.36%)

--- my-workspace/0015-3sum/solution.cpp
+++ my-workspace/0015-3sum/solution.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<vector<int>> triplets;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] > 0) break;
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
-            int j = i+1;
-            int k = nums.size()-1;
+            int j=i+1;
+            int k=n-1;
             while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
-                    triplets.push_back({nums[i], nums[j], nums[k]});
+                    ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                    while(j < k && nums[j] == nums[j-1]){
-                        j++;
-                    }
-                }
-                else if(sum < 0){
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(k > j && nums[k] == nums[k+1]) k--;
+                } else if(sum < 0){
                     j++;
-                }
-                else{
+                } else {
                     k--;
                 }
             }
         }
-        return triplets;
+        return ans;
     }
 };
