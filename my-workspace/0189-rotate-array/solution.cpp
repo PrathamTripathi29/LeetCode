7503a3e1fef368deec835a350486435282062c8e
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-04 15:07:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 29.7 MB (43.41%)

--- my-workspace/0189-rotate-array/solution.cpp
+++ my-workspace/0189-rotate-array/solution.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int size = nums.size();
-        k = k % size;
-        vector<int> temp;
-        for(int i = (size - k); i<size; i++){
-            int a = nums[i];
-            temp.push_back(a);
-        }
-        for(int i = (size - k - 1); i>=0; i--){
-            nums[i+k] = nums[i];
-        }
-        for(int i=0; i<k; i++){
-            nums[i] = temp[i];
-        }
+        int n = nums.size();
+        k = k%n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
