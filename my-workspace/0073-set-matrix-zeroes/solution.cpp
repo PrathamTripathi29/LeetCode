7a75a916700a50b1746715cc508532084c5b502d
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-05 07:21:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21 MB (29.73%)

--- my-workspace/0073-set-matrix-zeroes/solution.cpp
+++ my-workspace/0073-set-matrix-zeroes/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> rowcol;
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
+        unordered_set<int> rows, cols;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    rowcol.push_back({i, j});
+                    rows.insert(i);
+                    cols.insert(j);
                 }
             }
         }
-        for(int k=0; k<rowcol.size(); k++){
-            int i = rowcol[k][0];
-            int j = rowcol[k][1];
-            for(int a=0; a<matrix.size(); a++){
-                matrix[a][j] = 0;
+        for(int row : rows){
+            for(int i=0; i<n; i++){
+                matrix[row][i] = 0;
             }
-            for(int a=0; a<matrix[0].size(); a++){
-                matrix[i][a] = 0;
+        }
+        for(int col : cols){
+            for(int i=0; i<m; i++){
+                matrix[i][col] = 0;
             }
         }
     }
