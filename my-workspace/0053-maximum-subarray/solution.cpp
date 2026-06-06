442ce69840f78d7ee7d116654962731140cdd254
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-06 05:36:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.7 MB (80.41%)

--- my-workspace/0053-maximum-subarray/solution.cpp
+++ my-workspace/0053-maximum-subarray/solution.cpp
@@ -4,7 +4,8 @@ public:
         int sum = nums[0];
         int currsum = 0;
         for(int num : nums){
-            currsum = max(num, currsum+num);
+            if(currsum + num < num) currsum = num;
+            else currsum += num;
             sum = max(sum, currsum);
         }
         return sum;
