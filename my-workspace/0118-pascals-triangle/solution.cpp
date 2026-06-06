692d57ab5fef0cb733e810f8e971a6dddac4c482
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-06 05:48:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (30.24%)

--- my-workspace/0118-pascals-triangle/solution.cpp
+++ my-workspace/0118-pascals-triangle/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> pascal;
+        vector<vector<int>> ans;
         vector<int> dp;
         for(int i=0; i<numRows; i++){
             vector<int> temp(i+1, 1);
             for(int j=1; j<i; j++){
                 temp[j] = dp[j-1] + dp[j];
             }
-            pascal.push_back(temp);
+            ans.push_back(temp);
             dp = temp;
         }
-        return pascal;
+        return ans;
     }
 };
