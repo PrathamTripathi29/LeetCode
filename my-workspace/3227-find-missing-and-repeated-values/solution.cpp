f4079149761df5f7424674ec152d1bcce99b6067
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-08 05:31:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.6 MB (83.43%)

--- my-workspace/3227-find-missing-and-repeated-values/solution.cpp
+++ my-workspace/3227-find-missing-and-repeated-values/solution.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    // 1, 2, 3, 4, 5
+    // 1, 2, 2, 4, 5
+    // ideal - actual = m - d
+    // idea2 - actual2 = m2 - d2 = (m+d)(m-d)
+    // add = m+d
+    // diff = m-d
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        long long k = n*n;
-        long long ess = k * (k + 1) * (2*k + 1) / 6;
-        long long es = k * (k + 1) / 2;
-        long long ass = 0, as = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                as += grid[i][j];
-                ass += grid[i][j] * grid[i][j];
+        long long sum = 0;
+        long long sqSum = 0;
+        long long idealSum = 0;
+        long long idealSqSum = 0;
+        int n = grid.size()*grid.size();
+        idealSum = (long long)n*(n+1)/2;
+        idealSqSum = (long long)n*(n+1)*(2*n+1)/6;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid.size(); j++){
+                sum += grid[i][j];
+                sqSum += grid[i][j]*grid[i][j];
             }
         }
-        int r, m;
-        r = ((ess - ass)/(es - as) - (es - as))/2;
-        m = (es - as) + r;
+        long long diff = idealSum - sum;
+        long long diff2 = idealSqSum - sqSum;
+        long long add = diff2/diff;
+        long long m = (add+diff)/2;
+        long long d = (add-diff)/2;
         vector<int> ans;
-        ans.push_back(r);
+        ans.push_back(d);
         ans.push_back(m);
         return ans;
     }
