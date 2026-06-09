240f90bc2cf273d85d56b21f5601444fea014219
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-09 13:25:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 95.2 MB (61.23%)

--- my-workspace/0004-median-of-two-sorted-arrays/solution.cpp
+++ my-workspace/0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int med1, med2, median=0;
-        int count = 0, current;
-        int size = nums1.size() + nums2.size();
-        if(size%2 == 0){
-            med1 = (size-1)/2;
-            med2 = size/2;
+        if(nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int low = 0;
+        int high = n1;
+        while(low <= high){
+            int cut1 = low + (high-low)/2;
+            int cut2 = (n1 + n2 + 1)/2 - cut1;
+
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+
+            int r1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int r2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+            if(l1 <= r2 && l2 <= r1){
+                if((n1+n2)&1){
+                    return max(l1, l2);
+                } else {
+                    return (max(l1, l2) + min(r1, r2))/2.0;
+                }
+            } else if(l1 > r2){
+                high = cut1-1;
+            } else {
+                low = cut1+1;
+            }
         }
-        else{
-            med1 = med2 = size/2;
-        }
-        int i=0, j=0;
-        while(count <= med2){
-            if(i < nums1.size() && (j>= nums2.size() || nums1[i] <= nums2[j])){
-                current = nums1[i];
-                i++;
-            }
-            else{
-                current = nums2[j];
-                j++;
-            }
-            if(count == med1){
-                median += current;
-            }
-            if(count == med2){
-                median += current;
-            }
-            count++;
-        }
-        
-        return (double)median/2;
+        return 0.0;
     }
 };
