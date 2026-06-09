d2f0cd82295fa4196a686497b8251249496816b2
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-09 06:52:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (28.92%)

--- my-workspace/1646-kth-missing-positive-number/solution.cpp
+++ my-workspace/1646-kth-missing-positive-number/solution.cpp
@@ -7,12 +7,12 @@ public:
         while(low <= high){
             int mid = low + (high-low)/2;
             int missing = arr[mid] - mid - 1;
-            if(missing < k){
-                low = mid+1;
-            } else {
+            if(missing >= k){
                 high = mid-1;
+            } else {
+                low = mid+1;
             }
         }
-        return low + k;
+        return k + high + 1;
     }
 };
