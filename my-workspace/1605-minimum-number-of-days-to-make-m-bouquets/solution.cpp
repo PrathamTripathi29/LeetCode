22ed22bebec953282ad66278c248ddefeeea090f
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-09 05:32:03 +0000

    Sync LeetCode submission Runtime - 20 ms (70.63%), Memory - 70.5 MB (77.75%)

--- my-workspace/1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ my-workspace/1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
+    bool check(int mid, vector<int>& bloomDay, int m, int k){
+        int bloomed = 0;;
+        int bouqets = 0;
+        for(int d : bloomDay){
+            if(d <= mid){
+                bloomed++;
+            } else {
+                bouqets += bloomed/k;
+                bloomed = 0;
+            }
+        }
+        bouqets += bloomed/k;
+        return bouqets >= m;
+    }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
-        if((long long)m*k > n) return -1;
-        int low = INT_MAX;
-        int high = INT_MIN;
-        for(int day : bloomDay){
-            low = min(low, day);
-            high = max(high, day);
-        }
+        if(n/k < m) return -1;
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+
         while(low <= high){
-            int mid = low + (high - low)/2;
-            int consecKBlooms = 0;
-            int consecBlooms = 0;
-            for(int i=0; i<n; i++){
-                if(bloomDay[i] <= mid){
-                    consecBlooms++;
-                } else {
-                    consecKBlooms += consecBlooms/k;
-                    consecBlooms = 0;
-                }
-            }
-            consecKBlooms += consecBlooms/k;
-            if(consecKBlooms >= m){
-                high = mid - 1;
+            int mid = low + (high-low)/2;
+            if(check(mid, bloomDay, m, k)){
+                high = mid-1;
             } else {
-                low = mid + 1;
+                low = mid+1;
             }
         }
         return low;
