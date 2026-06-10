7441b0e99098ad447b0d81f43ff60ad7fe55df90
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-10 06:30:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (49.32%)

--- my-workspace/0242-valid-anagram/solution.cpp
+++ my-workspace/0242-valid-anagram/solution.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> mp;
-        if(s.length() != t.length()){
-            return false;
+        int n = s.length();
+        if(t.length() != n) return false;
+        vector<int> schar(26, 0);
+        for(int i=0; i<n; i++){
+            int chars = s[i] - 'a';
+            int chart = t[i] - 'a';
+            schar[chars]++;
+            schar[chart]--;
         }
-        for(int i = 0; i<s.length(); i++){
-            char ch = s[i];
-            if(mp.find(ch) == mp.end()){
-                mp[ch] = 1;
-            }
-            else{
-                mp[ch]++;
-            }
-        }
-        for(int i = 0; i<t.length(); i++){
-            char ch = t[i];
-            if(mp.find(ch) == mp.end()){
-                return false;
-            }
-            else{
-                mp[ch]--;
-            }
-            if(mp[ch] < 0){
-                return false;
-            }
+        for(int i=0; i<26; i++){
+            if(schar[i] != 0) return false;
         }
         return true;
     }
