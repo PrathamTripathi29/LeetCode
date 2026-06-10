cf7b94520ed59915bf502d07890f74928ec5a562
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-10 21:31:42 +0000

    Sync LeetCode submission Runtime - 8 ms (72.84%), Memory - 9.3 MB (80.20%)

--- my-workspace/0005-longest-palindromic-substring/solution.cpp
+++ my-workspace/0005-longest-palindromic-substring/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int checkpalindrome(string s, int l, int r){
-        while(l>=0 && r < s.length() && s[l] == s[r]){
+    int f(int l, int r, string& s, int n){
+        while(l >= 0 && r < n && s[l] == s[r]){
             l--;
             r++;
         }
         return r-l-1;
     }
     string longestPalindrome(string s) {
-        int start = 0;
-        int end = 0;
-        for(int i=0; i<s.length(); i++){
-            int maxlen = max(checkpalindrome(s, i, i), checkpalindrome(s, i, i+1));
-            if(maxlen > end - start){
-                start = i - (maxlen-1)/2;
-                end = i + maxlen/2;
+        int n = s.length();
+        int st = 0;
+        int ed = 0;
+        int maxLen = 0;
+        for(int i=0; i<n; i++){
+            int len = max(f(i, i, s, n), f(i, i+1, s, n));
+            if(len > maxLen){
+                maxLen = len;
+                st = i - (maxLen-1)/2;
+                ed = i + maxLen/2;
             }
         }
-        return s.substr(start, end-start+1);
+        return s.substr(st, ed-st+1);
     }
 };
