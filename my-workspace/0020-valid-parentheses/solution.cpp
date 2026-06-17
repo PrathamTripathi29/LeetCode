702b27c177c842104e0130300415d28ab18bebcc
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-17 15:27:00 +0000

    Sync LeetCode submission Runtime - 3 ms (9.39%), Memory - 8.9 MB (37.60%)

--- my-workspace/0020-valid-parentheses/solution.cpp
+++ my-workspace/0020-valid-parentheses/solution.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> S;
-        map<char, char> braces;
-        braces['('] = ')';
-        braces['{'] = '}';
-        braces['['] = ']';
-        for(int i=0; i<s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                S.push(s[i]);
-            }
-            else if(S.empty()){
-                return false;
-            }
-            else{
-                if(braces[S.top()] == s[i]){
-                    S.pop();
-                }
-                else{
-                    S.push(s[i]);
+        stack<char> st;
+        for(char ch : s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            } else {
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if(ch == ')'){
+                    if(top != '(') return false;
+                } else if(ch == '}'){
+                    if(top != '{') return false;
+                } else {
+                    if(top != '[') return false;
                 }
             }
         }
-        if(S.empty()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        if(st.empty()) return true;
+        return false;
     }
 };
