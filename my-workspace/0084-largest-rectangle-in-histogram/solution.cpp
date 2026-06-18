a43cfce376b23befe644a111b4a57050b43239ed
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-18 15:39:08 +0000

    Sync LeetCode submission Runtime - 20 ms (64.04%), Memory - 85.7 MB (42.84%)

--- my-workspace/0084-largest-rectangle-in-histogram/solution.cpp
+++ my-workspace/0084-largest-rectangle-in-histogram/solution.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
+        vector<int> nextSmaller(n);
+        vector<int> prevSmaller(n);
         stack<int> s;
-        vector<int> nextSmaller(n,n);
-        vector<int> prevSmaller(n, -1);
         for(int i=0; i<n; i++){
-            while(!s.empty() && heights[s.top()] > heights[i]){
-                nextSmaller[s.top()] = i;
+            while(!s.empty() && heights[s.top()] >= heights[i]){
                 s.pop();
             }
+            prevSmaller[i] = s.empty() ? -1 : s.top();
             s.push(i);
         }
         while(s.size()) s.pop();
-        for(int i= n-1; i>=0; i--){
-            while(!s.empty() && heights[s.top()] > heights[i]){
-                prevSmaller[s.top()] = i;
+        for(int i=n-1; i>=0; i--){
+            while(!s.empty() && heights[s.top()] >= heights[i]){
                 s.pop();
             }
+            nextSmaller[i] = s.empty() ? n : s.top();
             s.push(i);
         }
         int ans = 0;
         for(int i=0; i<n; i++){
-            ans = max(ans, (nextSmaller[i] - prevSmaller[i] - 1)*heights[i]);
+            int l = prevSmaller[i];
+            int r = nextSmaller[i];
+            ans = max(ans, (r-l-1)*heights[i]);
         }
         return ans;
     }
