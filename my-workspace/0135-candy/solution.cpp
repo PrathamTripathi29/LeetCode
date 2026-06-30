5094b0fef8175a8f35fcccee4da102b5846d3eb3
PrathamTripathi <prathamtripathi29@gmail.com>
2026-06-30 17:57:50 +0000

    Sync LeetCode submission Runtime - 1 ms (33.37%), Memory - 22.9 MB (67.88%)

--- my-workspace/0135-candy/solution.cpp
+++ my-workspace/0135-candy/solution.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
+        int count = 0;
         vector<int> candies(n, 0);
         candies[0] = 1;
         for(int i=1; i<n; i++){
             if(ratings[i] > ratings[i-1]){
                 candies[i] = candies[i-1] + 1;
-            } else{
+            } else {
                 candies[i] = 1;
             }
         }
-        for(int i = n-2; i>=0; i--){
+        for(int i=n-2; i>=0; i--){
             if(ratings[i] > ratings[i+1]){
-                candies[i] = max(candies[i], candies[i+1] + 1);
+                if(candies[i] <= candies[i+1]) candies[i] = candies[i+1] + 1;
             }
         }
-        int sum = 0;
-        for(int candy : candies){
-            sum += candy;
+        for(int i=0; i<n; i++){
+            count += candies[i];
         }
-        return sum;
+        return count;
     }
 };
