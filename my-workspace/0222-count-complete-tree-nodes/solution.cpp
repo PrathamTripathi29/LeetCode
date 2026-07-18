3271e0f846010723faa9d75362f3dc27a4625776
PrathamTripathi <prathamtripathi29@gmail.com>
2026-07-18 12:51:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.4 MB (15.74%)

--- my-workspace/0222-count-complete-tree-nodes/solution.cpp
+++ my-workspace/0222-count-complete-tree-nodes/solution.cpp
@@ -11,12 +11,19 @@
  */
 class Solution {
 public:
+    int lheight(TreeNode* root){
+        if(!root) return 0;
+        return lheight(root->left) + 1;
+    }
+    int rheight(TreeNode* root){
+        if(!root) return 0;
+        return rheight(root->right) + 1;
+    }
     int countNodes(TreeNode* root) {
-        if(!root){
-            return 0;
-        }
-        int l = countNodes(root->left);
-        int r = countNodes(root->right);
-        return 1 + l + r;
+        if(!root) return 0;
+        int lh = lheight(root);
+        int rh = rheight(root);
+        if(lh == rh) return (1<<lh) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
