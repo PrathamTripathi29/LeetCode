0d48cc7c2e3be98332f21d6414893236f7335d4c
PrathamTripathi <prathamtripathi29@gmail.com>
2026-07-19 04:56:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (53.05%)

--- my-workspace/0114-flatten-binary-tree-to-linked-list/solution.cpp
+++ my-workspace/0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -11,32 +11,13 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int> &preord){
-        if(root){
-            preord.push_back(root->val);
-            preorder(root->left, preord);
-            preorder(root->right, preord);
-        }
-    }
-    TreeNode* helper(TreeNode* root, vector<int> &preord){
-        TreeNode* ori = root;
-        for(int i=0; i<preord.size()-1; i++){
-            root->val = preord[i];
-            root->left = NULL;
-            root->right = new TreeNode;
-            root = root->right;
-        }
-        root->val = preord[preord.size()-1];
-        root->left = root->right = NULL;
-        return ori;
-    }
+    TreeNode* prev = nullptr;
     void flatten(TreeNode* root) {
-        if(!root){
-            return;
-        }
-        vector<int> preord;
-        preorder(root, preord);
-        root = helper(root, preord);
-        return;
+        if(!root) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right = prev;
+        root->left = nullptr;
+        prev = root;
     }
 };
