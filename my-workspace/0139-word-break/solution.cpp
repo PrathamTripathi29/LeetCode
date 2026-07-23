1619e083840063779c6891397640ddd1b04778c6
PrathamTripathi <prathamtripathi29@gmail.com>
2026-07-23 10:49:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.7 MB (84.02%)

--- my-workspace/0139-word-break/solution.cpp
+++ my-workspace/0139-word-break/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n = wordDict.size();
-        int len = s.length();
-        vector<bool> dp(len+1, false);
+        int n = s.length();
+        vector<int> dp(n+1, false);
         dp[0] = true;
-        for(int i=1; i<=len; i++){
-            for(auto word : wordDict){
-                int idx = i-word.length();
+        for(int i=1; i<=n; i++){
+            for(string word : wordDict){
+                int idx = i - word.length();
                 if(idx >= 0 && dp[idx] == true){
-                    string str = s.substr(idx, word.length());
-                    if(str == word){
+                    if(s.substr(idx, word.length()) == word){
                         dp[i] = true;
                         break;
                     }
                 }
             }
         }
-        return dp[len];
+        return dp[n];
     }
 };
